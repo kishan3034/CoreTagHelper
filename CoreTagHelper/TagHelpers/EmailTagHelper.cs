1fec641c224eb348d5d09653550ed44d442bf05f
kishan.anem <kishan3034@gmail.com>
2018-05-11 18:48:17 +0530

    Add project files.

---
+++ CoreTagHelper/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreTagHelper
+{
+    [HtmlTargetElement("email", TagStructure = TagStructure.WithoutEndTag)]
+    public class EmailTagHelper : TagHelper
+    {
+        private const string EmailDomain = "contoso.com";
+
+        // Can be passed via <email mail-to="..." />. 
+        // Pascal case gets translated into lower-kebab-case.
+        public string MailTo { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            var address = MailTo + "@" + EmailDomain;
+            output.Attributes.SetAttribute("href", "mailto:" + address);
+            output.Content.SetContent(address);
+        }
+    }
+
+    [HtmlTargetElement("kasyap")]
+    public class kishanTagHelper : TagHelper
+    {
+        private const string EmailDomain = "contoso.com";
+
+        // Can be passed via <email mail-to="..." />. 
+        // Pascal case gets translated into lower-kebab-case.
+        public string MailTo { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            var address = MailTo + "@" + EmailDomain;
+            output.Attributes.SetAttribute("href", "mailto:" + address);
+            output.Content.SetContent(address);
+        }
+    }
+
+}
